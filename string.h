ce545ac83e0a0f47db2f3bb244e0f84b11c823df
ian <pellet.ian1@gmail.com>
2020-04-06 15:19:13 +0200

    defined the attributes and methods needed for student C

--- string.h
+++ string.h
@@ -1,5 +1,16 @@
+#include <memory> // needed to use an allocator
+
 class string{
 	public :
+		~string(); // destructor
+		int capacity(); // returns the memory allocated to the array of char
+		bool empty(); // true if length = 0
+		reserve(size_t n); // allocate or desallocate memory in order to have a capacity of n
+		string& operator=(const char* s); // assigns the value s to the string
+		string operator+(const string& lhr, const string& rhs); // returns the concatenation of lhr adn rhs
 
 	protected :
+		char* string_; // pointer to the array containing the char of our string
+		allocator<char> alloc_; // will be used to manage the string's memory space
+		int capacity_; // needs to be updated each time we allocate or desallocate memory space
 };
