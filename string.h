7009da676c60f7c0bc310706487fa8d787e1fe77
jpludowski <62611061+jpludowski@users.noreply.github.com>
2020-04-20 18:03:06 +0200

    member A methods, repaired

--- string.h
+++ string.h
@@ -29,23 +29,24 @@ class string{
 		string operator+(const char* a);
 
 		//Member B
-		//string(char &string);	//constructor from a c-sting
-        //size_t length();
-        //size_t max_size();
-        //void resize(size_t n, char c);
-        //string operator=(const string&);
-        //string operator+(const string&);
-        //string operator=(const string string&);
+		string(char* c_string);	//constructor from a c-sting
+        	int length();
+       		//size_t max_size();
+       		void resize(size_t n, char c);
+        	//string operator=(const string&);
+        	//string operator+(const string&);
+        	//string operator=(const string string&);
 		//string operator+(char c);
-		
-		
+
+
 		// Member C
 		~string(); // destructor
 		size_t capacity(); // returns the memory allocated to the array of char
-		//bool empty(); // true if length = 0
+		bool empty(); // true if length = 0
 		//reserve(size_t n); // allocate or desallocate memory in order to have a capacity of n
 		//string& operator=(const char* s); // assigns the value s to the string
 		//string operator+(const string& lhr, const string& rhs); // returns the concatenation of lhr adn rhs
+
 	protected :
 		char* string_; // dynamic array containing the string's chars
 		size_t capacity_; // needs to be updated each time we allocate or desallocate memory space
