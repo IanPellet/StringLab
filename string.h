c23fe1c28e9fbda7cf0788fc1b79e1ce07422fdd
Jakub Maciej Płudowski <j.pludowski@stud.elka.pw.edu.pl>
2020-04-24 16:08:10 +0200

    change-check

--- string.h
+++ string.h
@@ -1,47 +1,55 @@
-/*struct node    // node which will contain a letter and a pointer to next node with next letter
+//change
+typedef unsigned int size_t;
+int char_length(char * a)		//function which will count the length of char*
 {
-	char value;			//letter
-	node*next = nullptr;		//pointer to next node
-
-};*/
-
-typedef long unsigned int size_t;
-
+	// checking the length of parameter "a"
+	int counter = 0;
+	char n = a[counter];
+	while (n != '\0')
+	{
+		counter++;
+		n = a[counter];
+	}
+	return counter;
+	// counter is the length of parameter "a"
+}
 
 class string{
 	public :
+		static int HowManyStrings();
 		string(); // constructor
+		
 
 		// Member A
-		//µstring(const string a); //copy constructor
-		char* c_str();
-		//int size(); //method which will count number of nodes in string
-		//void clear(); //method which will delete all nodes and set first pointer to null
-		//string& operator=(char a);
-		//string operator+(const string&, const char*);
+		string(const string& a);  //copy constructor
+		char* c_str(); 
+		size_t size(); //method which will count number of chars in string
+		void clear(); //method which will delete all chars 
+		string& operator=(char* a);
+		string operator+(const char* a);
 
 		//Member B
-		string(char* c_string);	//constructor from a c-sting
-        int length();
+		//string(char &string);	//constructor from a c-sting
+        //size_t length();
         //size_t max_size();
         //void resize(size_t n, char c);
         //string operator=(const string&);
         //string operator+(const string&);
         //string operator=(const string string&);
 		//string operator+(char c);
-
-
+		
+		
 		// Member C
 		~string(); // destructor
 		size_t capacity(); // returns the memory allocated to the array of char
-		bool empty(); // true if length = 0
+		//bool empty(); // true if length = 0
 		//reserve(size_t n); // allocate or desallocate memory in order to have a capacity of n
 		//string& operator=(const char* s); // assigns the value s to the string
 		//string operator+(const string& lhr, const string& rhs); // returns the concatenation of lhr adn rhs
 	protected :
 		char* string_; // dynamic array containing the string's chars
-		size_t capacity_ = 0; // needs to be updated each time we allocate or desallocate memory space
-
-	//private:
-	//	node * first = nullptr;			//first pointer to the first node (letter) which is empty in the beggining
+		size_t capacity_; // needs to be updated each time we allocate or desallocate memory space
+		int length;
+	private:
+		static int num_strings;
 };
--- string_tests.cpp
+++ string_tests.cpp
@@ -1,18 +1,14 @@
-#include "string.cpp"
+#include "string.h"
 #include <iostream>
-#include <iterator>
 
 int main()
 {
 	// Dummy constructor test (can be deleted once a real constructor is implemented)
-	//string hello();
-	//std::cout << hello.c_str() << std::endl;
+	string hello;
+	std::cout << hello.c_str() << std::endl;
 
-// Member C
 	// capacity test (will need to be reimplemented once a real constructor have been made)
-	char helloc[] = "Hello";
-	string hello(helloc);
-	bool capatest = hello.capacity() == std::end(helloc) - std::begin(helloc) - 1;
+	bool capatest = hello.capacity()==12;
 	std::cout << "Capacity : ";
 	if(capatest){
 		std::cout << "ok" << std::endl;
@@ -20,18 +16,6 @@ int main()
 		std::cout << "not working" << std::endl;
 	}
 
-	// empty test
-	char heyc[] = "hey";
-	char oc[] = "";
-	string hey(heyc);
-	string o(oc);
-	bool emptytest = (o.empty() && !hey.empty());
-	std::cout << "Empty : ";
-	if(emptytest){
-		std::cout << "ok" << std::endl;
-	}else{
-		std::cout << "not working" << std::endl;
-	}
 
 	return 0;
 }
