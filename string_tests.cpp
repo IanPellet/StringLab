b9604b9b9be2d645fd0fee07be7171f16b31c24c
Nino155 <62557495+Nino155@users.noreply.github.com>
2020-04-27 17:31:46 +0200

    Update string_tests.cpp

--- string_tests.cpp
+++ string_tests.cpp
@@ -5,9 +5,139 @@
 
 int main()
 {
-	// Dummy constructor test (can be deleted once a real constructor is implemented)
-	//string hello();
-	//std::cout << hello.c_str() << std::endl;
+
+	
+	
+	
+//member B :
+	
+	  //test constructor from c_string : 
+
+	  char str_c[]="hello";
+	  string test(str_c);
+	  char* s = test.c_str();
+	  bool content_test=true;
+	  int i = 0;
+	  while(str_c[i]!='\0')   //comparaison des composant des chaines 1 à 1
+	  {
+	    if (str_c[i]!=s[i])
+	      content_test=false;
+	    i++;
+	  }
+
+	  if(content_test)
+	    std::cout<<"Même contenu entre le c_string et notre string issu du constructeur, constructeur ok"<<std::endl;
+	  else
+	    std::cout<<"contenu différent entre le c_string et notre string issu du constructeur, constructeur pas ok"<<std::endl;
+
+	  bool capa_test = (test.capacity()==i);  //la capacité du string est-elle bien initialisée avec la taille du c_string ?
+
+	  if(capa_test)
+	    std::cout<<"La capacité est égale à la taille du c_string, constructeur ok"<<std::endl;
+	  else
+	    std::cout<<"La capacité n'est pas égale à la taille du c_string, constructeur pas ok"<<std::endl;
+
+
+
+	//test fonction resize
+
+	  //cas ou on resize en plus petit
+	char test1[]="hello";
+	char test2[]="hel";
+	char test3[]="helloccccc";
+
+	string str1(test1);
+	string str2(test2);
+	string str3(test3);
+	str1.resize(3,'c'); //str1 = str2 si resize fonctionne bien
+	bool test_resize1 = strcmp(str1.c_str(),str2.c_str());  //renvoi 0 si chaines égales
+	if(test_resize1 == 0)
+	  std::cout<<"les chaines de carractère sont égales, resize en plus petit ok"<<std::endl;
+	else
+	  std::cout<<"les chaines sont différentes, resize en plus petit pas ok"<<std::endl;
+	test_resize1=(str1.capacity()==str2.capacity());
+	if(test_resize1)
+	  std::cout<<"Les capacités sont égales, resize en plus petit ok"<< std::endl;
+	else
+	  std::cout<<"Les capacités sont différentes, resize en plus petit pas ok"<<std::endl;
+
+
+	  //cas ou on resize en plus grand
+
+	char test4[]="hello";
+	string str4(test4);
+	str4.resize(10,'c'); //str4 = str3 si resize fonctionne bien
+
+	bool test_resize2 = strcmp(str4.c_str(),str3.c_str());  //renvoi 0 si chaines égales
+	if(test_resize2 == 0)
+	  std::cout<<"les chaines de carractère sont égales, resize en plus grand ok"<<std::endl;
+	else
+	  std::cout<<"les chaines sont différentes, resize en plus grand pas ok"<<std::endl;
+	test_resize2=(str3.capacity()==str4.capacity());
+	if(test_resize2)
+	  std::cout<<"Les capacités sont égales, resize en plus grand ok"<< std::endl;
+	else
+	  std::cout<<"Les capacités sont différentes, resize en plus grand pas ok"<<std::endl;
+
+
+	//test fonction length()
+
+	char test_len[]="heyy";
+	string test_l(test_len);
+	bool len_test = (test_l.length()==4);
+	if(len_test)
+	  std::cout<<"La longueur est la bonne, length() ok"<< std::endl;
+	else
+	  std::cout<<"La longueur est fausse, length() pas ok"<<std::endl;
+
+
+	//test opérateur = entre deux string :
+
+	char test_op1[]="hello";
+	char test_op2[]="hi";
+	string test_o1(test_op1);
+	string test_o2(test_op2);
+	test_o1=test_o2;  
+	bool content_test2 = strcmp(test_o1.c_str(),test_o2.c_str());
+	if(content_test2 == 0)
+	  std::cout<<"les chaines de carractère sont égales, l'opérateur = fonctionne"<<std::endl;
+	else
+	  std::cout<<"les chaines sont différentes, opérateur = défectueux"<<std::endl;
+
+	bool capa_test2 = (test_o1.capacity()==test_o2.capacity());
+	if(capa_test2)
+	  std::cout<<"La longueur est égale, opérateur = fonctionnel"<< std::endl;
+	else
+	  std::cout<<"La longueur est différente, opérateur = défectueux"<<std::endl;
+
+	//test opérateur + entre un string et un char : 
+	char test_ope1[]="hello";
+	char test_ope2[]="helloc";
+	string test_oe1(test_ope1);
+	string test_oe2(test_ope2);
+	test_oe1=test_oe1 + 'c';
+
+	bool content_test3 = strcmp(test_oe1.c_str(),test_oe2.c_str());
+	if(content_test3 == 0)
+	  std::cout<<"les chaines de carractère sont égales, l'opérateur + fonctionne"<<std::endl;
+	else
+	  std::cout<<"les chaines sont différentes, opérateur + défectueux"<<std::endl;
+
+	bool capa_test3 = (test_oe1.capacity()==test_oe2.capacity());
+	if(capa_test3)
+	  std::cout<<"La longueur est égale, opérateur + fonctionnel"<< std::endl;
+	else
+	  std::cout<<"La longueur est différente, opérateur + défectueux"<<std::endl;  
+
+
+	
+	
+	
+	
+	
+	
+	
+	
 
 // Member C
 	// capacity test
